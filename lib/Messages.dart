04e36748ec3c387523cfb6a2217acab6d512c11e
pdaniel37 <pedrodaniel@candev.com.br>
2019-12-31 10:35:35 -0300

    Adding box message

--- lib/Messages.dart
+++ lib/Messages.dart
@@ -11,14 +11,78 @@ class Messages extends StatefulWidget {
 }
 
 class _MessagesState extends State<Messages> {
+  TextEditingController _controllerMessage = TextEditingController();
+
+  _sendMessage(){
+
+  }
+
+  _sendImage(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    var messageBox = Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: TextField(
+                controller: _controllerMessage,
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                    hintText: "Digite uma mensagem...",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                  prefixIcon: IconButton(
+                    icon: Icon(Icons.camera_alt),
+                    onPressed: _sendImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            backgroundColor: Color(0xff075E54),
+            child: Icon(Icons.send, color: Colors.white,),
+            mini: true,
+            onPressed: _sendMessage,
+          )
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.contact.name),
       ),
       body: Container(
-
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/bg.png"),
+            fit: BoxFit.cover
+          )
+        ),
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                Text("list view"),
+                messageBox
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
